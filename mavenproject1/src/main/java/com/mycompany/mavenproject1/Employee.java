77d0d9adc194cb380269364a1121106b62fc6ef9
sweta09876 <116981671+sweta09876@users.noreply.github.com>
2022-11-09 18:47:13 -0500

    initial commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Employee.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Sweta
+ */
+public class Employee {
+  private int id;
+private String name;
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", name=" + name + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Employee() {
+    }
+
+}
---
+++ mavenproject1/src/test/java/test/com/EmployeeTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com;
+
+import com.mycompany.mavenproject1.Employee;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Sweta
+ */
+public class EmployeeTest {
+  Employee emp;  
+    public EmployeeTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception { 
+        emp = new Employee();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    } 
+    @Test
+    public void testName(){
+        emp.setName("kevin");
+        assertEquals("kevin", emp.getName());
+    }
+    
+    
+}
